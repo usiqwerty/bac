dfed03ff52a2b08b8ce4e576b90dcdbcef02da8a
usiqwerty <benlogin1@protonmail.com>
2022-02-05 15:29:36 +0500

    User input

--- bac.py
+++ bac.py
@@ -3,59 +3,73 @@
 ALCOHOL_DENSITY: float = 0.8  #: density of alcohol (g/ml)
 BLOOD_DENSITY: float = 1.055  #: density of blood (g/ml)
 WATER_IN_BLOOD: float = 0.8  #: parts of water in blood (%)
-ALCOHOL_DEGRADATION: float = 0.0025  #: for kg body weight per minute (g)
+ALCOHOL_DEGRADATION: float = 0.002  #: for kg body weight per minute (g)
 
-ALCOHOL_DEGRADATION_WARNING = """
-In the next version the value used for alcohol degradation will be lowered. To
-keep the current default, you can override `ALCOHOL_DEGRADATION`, or use the new
-*degradation* argument to the `calculate_alcohol_degradation` and
-`get_blood_alcohol_degradation` functions.
-"""
-
-
-def alcohol_to_ethanol(*, volume: int, percent:float):
-	return ALCOHOL_DENSITY * volume * (percent / 100)
-
-def calculate_alcohol_degradation(*, weight: int, minutes: int = 1, degradation: Optional[float] = None) -> float:
-	if degradation is None:
-		degradation = ALCOHOL_DEGRADATION
-	return degradation * weight * minutes
-
-
-def body_water(*, age: int, weight: int, height: int, female: bool):
-	if female:
-		return 0.203 - (0.07 * age) + (0.1069 * height) + (0.2466 * weight)
-	else:  # male
-		return 2.447 - (0.09516 * age) + (0.1074 * height) + (0.3362 * weight)
-
+#given alcohol gramms
+#body water in liter
 
 def promille_to_gramm(*, promille: float, body_water: float) -> float:
 	return (promille * (BLOOD_DENSITY * body_water)) / WATER_IN_BLOOD
 
 
-def gramm_to_promille(*, gramm: float, body_water: float) -> float:
-	#given alcohol gramms
-	#body water in liter
+def get_blood_alcohol_content(age, weight, height, female, volume, percent):
+	gramm =  ALCOHOL_DENSITY * volume * (percent / 100)
+	if female:
+		body_water= 0.203 - (0.07 * age) + (0.1069 * height) + (0.2466 * weight)
+	else:
+		body_water= 2.447 - (0.09516 * age) + (0.1074 * height) + (0.3362 * weight)
+
 	return (gramm * WATER_IN_BLOOD) / (BLOOD_DENSITY * body_water)
 
 
-def get_blood_alcohol_content(age: int,weight: int, height: int, female: bool, volume: int, percent: float) -> float:
-	gramm = alcohol_to_ethanol(volume=volume, percent=percent)
-	tot_body_water = body_water(age=age, weight=weight, height=height, female=female)
-	return gramm_to_promille(gramm=gramm, body_water=tot_body_water)
+
+def get_blood_alcohol_degradation(age, weight, height, female, minutes = 1):
+	gramm = ALCOHOL_DEGRADATION * weight * minutes
+	if female:
+		body_water= 0.203 - (0.07 * age) + (0.1069 * height) + (0.2466 * weight)
+	else:
+		body_water= 2.447 - (0.09516 * age) + (0.1074 * height) + (0.3362 * weight)
+
+	permille= (gramm * WATER_IN_BLOOD) / (BLOOD_DENSITY * body_water)
+	return permille
+def between(x, a, b):
+	if x>=a and x<b:
+		return True
+	else:
+		return False
+
+if __name__=="__main__":
+	age=int(input("age: "))
+	weight=int(input("weight: "))
+	height=int(input("height: "))
+	female=True if input("r u female - y/n? ") == "y" else False
+	volume=int(input("volume: "))
+	percent=int(input("percent: "))
+	minutes=int(input("minutes since intake: "))
+	print(age,weight,height, female, volume, percent, minutes)
+	permille=get_blood_alcohol_content(age ,weight, height, female, volume, percent)
 
 
-def get_blood_alcohol_degradation(*,age: int,weight: int,height: int,female: bool,minutes: int = 1) -> float:
-	gramm = calculate_alcohol_degradation(weight=weight, minutes=minutes, degradation=ALCOHOL_DEGRADATION)
-	tot_body_water = body_water(age=age, weight=weight, height=height, female=female)
-	return gramm_to_promille(gramm=gramm, body_water=tot_body_water)
+	permill= max ( 0, permille-get_blood_alcohol_degradation(age, weight, height, female, minutes))
+	if permill<0.2:
+		msg="Трезвое состояние"
+	elif between(permill, 0.2, 0.3):
+		msg="В среднем поведение нормальное, Скрытые проявления, которые могут быть обнаружены специальными тестами"
+	elif between(permill, 0.3, 0.6):
+		msg="Средневыраженная эйфория, расслабление,ощущение радости, говорливость, понижение сдержанности, нарушение концентрации"
+	elif between(permill, 0.6, 1):
+		msg="Притупление ощущения, расторможенность, экстравертность, затруднено рассуждение, снижена глубина восприятия, нарушено периферическое зрение, нарушенное приспособление зрачка к свету"
+	elif between(permill, 1, 2):
+		msg="Сверх-экспрессивность, переменчивость эмоций, гнев или печаль, снижение либидо, заторможены рефлексы, увеличивается время реакции, теряется способность к контролю движения (появляется шатающаяся походка), нечленораздельная речь, временно нарушена эрекция, вероятность временного алкогольного отравления"
+	elif between(permill, 2, 3):
+		msg="Ступор, потеря способности к пониманию, ослабление способностей к ощущению, вероятность потери сознания, потеря сознания, потеря памяти"
+	elif between(permill, 3, 4):
+		msg="Сильное угнетение функций центральной нервной системы, потеря сознания, возможность смерти, теряется контроль над мочеиспусканием, нарушается дыхание, полная утрата чувства равновесия,нарушено сердцебиение"
+	elif between(permill, 4, 5):
+		msg="Полная утрата контроля за поведением, потеря сознания, вероятность смерти, проблемы с дыханием, нарушено сердцебиение, теряется контроль над движением зрачков (Нистагм)"
+	elif permill>5:
+		msg="Высокий риск отравления, возможность смерти"
+	#print ( round(permill, 4) )
+	print(msg)
+	print("after {} minutes: {} -> {}".format(minutes,round(permille, 4), round(permill, 4)) )
 
-
-age=30
-weight=80
-height=180
-female=False
-volume=100
-percent=40
-permille=get_blood_alcohol_content(age ,weight, height, female, volume, percent)
-print ( round(permille, 1) )
