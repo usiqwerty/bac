6d45c60bef645cab8c2cb8f1d4af6ecdd7d9672c
usiqwerty <benlogin1@protonmail.com>
2022-06-14 17:05:06 +0500

    get_blood_alcohol_content() and get_blood_alcohol_degradation() are merged into one
    removed unused promille_to_gramm()
    between() is replaced with simple python expression
    
    Renamed for better understanding:
    	calc() -> bac()
    	female -> is_female

--- bac.py
+++ bac.py
@@ -8,71 +8,58 @@ ALCOHOL_DEGRADATION: float = 0.002  #: for kg body weight per minute (g)
 #given alcohol gramms
 #body water in liter
 
-def promille_to_gramm(*, promille: float, body_water: float) -> float:
-	return (promille * (BLOOD_DENSITY * body_water)) / WATER_IN_BLOOD
+def get_blood_alcohol_content(age, weight, height, female, volume, percent, minutes):
 
+	if female:
+		body_water= 0.203 - (0.07 * age) + (0.1069 * height) + (0.2466 * weight)
+	else:
+		body_water= 2.447 - (0.09516 * age) + (0.1074 * height) + (0.3362 * weight)
 
-def get_blood_alcohol_content(age, weight, height, female, volume, percent):
 	gramm =  ALCOHOL_DENSITY * volume * (percent / 100)
-	if female:
-		body_water= 0.203 - (0.07 * age) + (0.1069 * height) + (0.2466 * weight)
-	else:
-		body_water= 2.447 - (0.09516 * age) + (0.1074 * height) + (0.3362 * weight)
+	maximum = (gramm * WATER_IN_BLOOD) / (BLOOD_DENSITY * body_water)
 
-	return (gramm * WATER_IN_BLOOD) / (BLOOD_DENSITY * body_water)
+	#turns into 0 if minutes are equal to 0
+	#so no condition needed
+	degrad_factor = ALCOHOL_DEGRADATION * weight * minutes
+	decrease = (degrad_factor * WATER_IN_BLOOD) / (BLOOD_DENSITY * body_water)
 
+	return maximum-decrease, maximum
 
+def bac(age ,weight, height, female, volume, percent, minutes):
+	current, maximum =  get_blood_alcohol_content(age, weight, height, female, volume, percent, minutes)
 
-def get_blood_alcohol_degradation(age, weight, height, female, minutes):
-	gramm = ALCOHOL_DEGRADATION * weight * minutes
-	if female:
-		body_water= 0.203 - (0.07 * age) + (0.1069 * height) + (0.2466 * weight)
-	else:
-		body_water= 2.447 - (0.09516 * age) + (0.1074 * height) + (0.3362 * weight)
+	current = max (0, current)	#it could be negative if minutes are too big
 
-	permille= (gramm * WATER_IN_BLOOD) / (BLOOD_DENSITY * body_water)
-	return permille
-def between(x, a, b):
-	if x>=a and x<b:
-		return True
-	else:
-		return False
-
-def calc(age ,weight, height, female, volume, percent, minutes):
-
-	maximum=get_blood_alcohol_content(age ,weight, height, female, volume, percent)
-
-	current = max ( 0, maximum-get_blood_alcohol_degradation(age, weight, height, female, minutes))
-	if  current<0.2:
+	if  current < 0.2:
 		msg="Трезвое состояние"
-	elif between(current, 0.2, 0.3):
+	elif 0.2 <= current < 0.3:
 		msg="В среднем поведение нормальное, Скрытые проявления, которые могут быть обнаружены специальными тестами"
-	elif between(current, 0.3, 0.6):
+	elif 0.3 <= current < 0.6:
 		msg="Средневыраженная эйфория, расслабление,ощущение радости, говорливость, понижение сдержанности, нарушение концентрации"
-	elif between(current, 0.6, 1):
+	elif 0.6 <= current < 1:
 		msg="Притупление ощущения, расторможенность, экстравертность, затруднено рассуждение, снижена глубина восприятия, нарушено периферическое зрение, нарушенное приспособление зрачка к свету"
-	elif between(current, 1, 2):
+	elif   1 <= current < 2:
 		msg="Сверх-экспрессивность, переменчивость эмоций, гнев или печаль, снижение либидо, заторможены рефлексы, увеличивается время реакции, теряется способность к контролю движения (появляется шатающаяся походка), нечленораздельная речь, временно нарушена эрекция, вероятность временного алкогольного отравления"
-	elif between(current, 2, 3):
+	elif   2 <= current < 3:
 		msg="Ступор, потеря способности к пониманию, ослабление способностей к ощущению, вероятность потери сознания, потеря сознания, потеря памяти"
-	elif between(current, 3, 4):
+	elif   3 <= current < 4:
 		msg="Сильное угнетение функций центральной нервной системы, потеря сознания, возможность смерти, теряется контроль над мочеиспусканием, нарушается дыхание, полная утрата чувства равновесия,нарушено сердцебиение"
-	elif between(current, 4, 5):
+	elif   4 <= current < 5:
 		msg="Полная утрата контроля за поведением, потеря сознания, вероятность смерти, проблемы с дыханием, нарушено сердцебиение, теряется контроль над движением зрачков (Нистагм)"
-	elif current>5:
+	elif current > 5:
 		msg="Высокий риск отравления, возможность смерти"
-	return (maximum, current, msg)
+	return maximum, current, msg
 if __name__=="__main__":
 	age=int(input("age: "))
 	weight=int(input("weight (kg): "))
 	height=int(input("height (cm): "))
-	female=True if input("r u female - y/n? ") == "y" else False
+	is_female=True if input("r u female - y/n? ") == "y" else False
 	volume=int(input("volume (ml): "))
 	percent=int(input("percent: "))
 	minutes=int(input("minutes since intake: "))
-	print(age,weight,height, female, volume, percent)
+	print(age,weight,height, is_female, volume, percent)
 
-	max, final, msg=calc(age ,weight, height, female, volume, percent, minutes)
+	max, final, msg = bac(age, weight, height, is_female, volume, percent, minutes)
 	#print ( round(permill, 4) )
 	print(msg)
 	print("after {} minutes: {} -> {}".format(minutes,round(max, 4), round(final, 4)) )
